e501d381c275e3cf0d0171cb089be361906540e8
Francis Appels <francis.appels@yahoo.com>
2023-01-13 09:10:39 +0100

    Fix block wrong reopen

--- htdocs/fourn/commande/card.php
+++ htdocs/fourn/commande/card.php
@@ -2426,18 +2426,14 @@ if ($action == 'create') {
 		if (empty($reshook)) {
 			$object->fetchObjectLinked(); // Links are used to show or not button, so we load them now.
 
-			// get reception status
+			// check if reception
 			$hasreception = 0;
-			$hasstartedreception = 0;
 			if ($conf->reception->enabled) {
-				$labelofbutton = $langs->trans("CreateReception");
 				if (!empty($object->linkedObjects['reception'])) {
 					foreach ($object->linkedObjects['reception'] as $element) {
 						if ($element->statut >= 0) {
 							$hasreception = 1;
-						}
-						if ($element->statut >= 1) {
-							$hasstartedreception = 1;
+							break;
 						}
 					}
 				}
@@ -2464,7 +2460,7 @@ if ($action == 'create') {
 
 			// Modify
 			if ($object->statut == CommandeFournisseur::STATUS_VALIDATED) {
-				if ($usercanorder && !$hasstartedreception) {
+				if ($usercanorder) {
 					print '<a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&action=reopen&token='.newToken().'">'.$langs->trans("Modify").'</a>';
 				}
 			}
@@ -2516,19 +2512,23 @@ if ($action == 'create') {
 			}
 
 			// Reopen
-			if (in_array($object->statut, array(CommandeFournisseur::STATUS_ACCEPTED)) && !$hasstartedreception) {
+			if (in_array($object->statut, array(CommandeFournisseur::STATUS_ACCEPTED))) {
 				$buttonshown = 0;
-				if (!$buttonshown && $usercanapprove) {
-					if (empty($conf->global->SUPPLIER_ORDER_REOPEN_BY_APPROVER_ONLY)
-						|| (!empty($conf->global->SUPPLIER_ORDER_REOPEN_BY_APPROVER_ONLY) && $user->id == $object->user_approve_id)) {
-						print '<a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&action=reopen&token='.newToken().'">'.$langs->trans("Disapprove").'</a>';
-						$buttonshown++;
+				if ($hasreception) {
+					print '<a class="butActionRefused classfortooltip" href="#" title="'.$langs->trans("ReceptionExist").'">'.$langs->trans("Disapprove").'</a>';
+				} else {
+					if (!$buttonshown && $usercanapprove) {
+						if (empty($conf->global->SUPPLIER_ORDER_REOPEN_BY_APPROVER_ONLY)
+							|| (!empty($conf->global->SUPPLIER_ORDER_REOPEN_BY_APPROVER_ONLY) && $user->id == $object->user_approve_id)) {
+							print '<a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&action=reopen&token='.newToken().'">'.$langs->trans("Disapprove").'</a>';
+							$buttonshown++;
+						}
 					}
-				}
-				if (!$buttonshown && $usercanapprovesecond && !empty($conf->global->SUPPLIER_ORDER_3_STEPS_TO_BE_APPROVED)) {
-					if (empty($conf->global->SUPPLIER_ORDER_REOPEN_BY_APPROVER2_ONLY)
-						|| (!empty($conf->global->SUPPLIER_ORDER_REOPEN_BY_APPROVER2_ONLY) && $user->id == $object->user_approve_id2)) {
-						print '<a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&action=reopen&token='.newToken().'">'.$langs->trans("Disapprove").'</a>';
+					if (!$buttonshown && $usercanapprovesecond && !empty($conf->global->SUPPLIER_ORDER_3_STEPS_TO_BE_APPROVED)) {
+						if (empty($conf->global->SUPPLIER_ORDER_REOPEN_BY_APPROVER2_ONLY)
+							|| (!empty($conf->global->SUPPLIER_ORDER_REOPEN_BY_APPROVER2_ONLY) && $user->id == $object->user_approve_id2)) {
+							print '<a class="butAction" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&action=reopen&token='.newToken().'">'.$langs->trans("Disapprove").'</a>';
+						}
 					}
 				}
 			}
@@ -2540,7 +2540,11 @@ if ($action == 'create') {
 
 			// Ship
 			if (!empty($conf->stock->enabled) && (!empty($conf->global->STOCK_CALCULATE_ON_SUPPLIER_DISPATCH_ORDER) || !empty($conf->global->STOCK_CALCULATE_ON_RECEPTION) || !empty($conf->global->STOCK_CALCULATE_ON_RECEPTION_CLOSE))) {
-				$labelofbutton = $langs->trans('ReceiveProducts');
+				if ($conf->reception->enabled) {
+					$labelofbutton = $langs->trans("CreateReception");
+				} else {
+					$labelofbutton = $langs->trans('ReceiveProducts');
+				}
 
 				if (in_array($object->statut, array(3, 4, 5))) {
 					if (((!empty($conf->fournisseur->enabled) && empty($conf->global->MAIN_USE_NEW_SUPPLIERMOD)) || !empty($conf->supplier_order->enabled)) && $usercanreceive) {
