880d1196a6b42d2f22d46f4e543517e34262d01b
Francis Appels <francis.appels@yahoo.com>
2023-04-28 10:42:39 +0200

    Add missing error message on modify order error.

--- htdocs/commande/card.php
+++ htdocs/commande/card.php
@@ -1236,6 +1236,8 @@ if (empty($reshook)) {
 
 					$object->generateDocument($model, $outputlangs, $hidedetails, $hidedesc, $hideref);
 				}
+			} else {
+				setEventMessages($object->error, $object->errors, 'errors');
 			}
 		}
 	} elseif ($action == 'confirm_shipped' && $confirm == 'yes' && $usercanclose) {
