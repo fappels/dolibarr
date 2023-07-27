1c938910842474f01e10e83389e336a7ccd299f9
Francis Appels <francis.appels@yahoo.com>
2023-07-27 17:33:12 +0200

    HAR-116 Missing show error on shipment card on re-open error.

--- htdocs/expedition/card.php
+++ htdocs/expedition/card.php
@@ -159,6 +159,9 @@ if (empty($reshook)) {
 	if ($action == 'reopen' && $user->rights->expedition->creer) {
 		$object->fetch($id);
 		$result = $object->reOpen();
+		if ($result < 0) {
+			setEventMessages($object->error, $object->errors, 'errors');
+		}
 	}
 
 	// Set incoterm
